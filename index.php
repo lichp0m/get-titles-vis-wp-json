bb4400f808ee97414b1cd381ef6174eea7bf4904
lichp0m <116561067+lichp0m@users.noreply.github.com>
2022-10-26 22:02:05 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,12 @@
 <?php
 
 $json = file_get_contents("https://spryt.ru/wp-json/wp/v2/posts?_fields=title"); //Получаю json-ответ, записываю в переменную
-$cleanedJson = stristr($json, "["); // Вычистил код ошибки из полученной переменной, который мешал декодировать JSON
-$decodedJson = json_decode($cleanedJson, true);
-//var_export($decodedJson);
+$cleanedJson = stristr($json, "["); // Вычистил код ошибки, который мешал декодировать JSON из полученной переменной
+$decodedJson = json_decode($cleanedJson, true); // Преобразовал юникод-символы в кириллицу
 
 foreach ($decodedJson as $key) {
       foreach ($key as $value) {
-          foreach ($value as $title) {
+          foreach ($value as $title) { // Обхожу вложенные массивы
               print_r($title . "\n");
           }
     }
