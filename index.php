2fe7a1fb30cfd0d3d336ca19255c1772526f1f02
lichp0m <116561067+lichp0m@users.noreply.github.com>
2022-10-26 21:58:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 $json = file_get_contents("https://spryt.ru/wp-json/wp/v2/posts?_fields=title"); //Получаю json-ответ, записываю в переменную
-$cleanedJson = str_replace('{"title":{', "\n", $json);
-$badchars = array("}]", "},", "[", "\"rendered\":", "}", "\""); // Массив символов для очистки кода для json_decode
-$cleanedJson = str_replace($badchars, "", $cleanedJson); // Очищаю получившиеся данные от символов, мешающих json_decode
+$cleanedJson = stristr($json, "["); // Вычистил код ошибки из полученной переменной, который мешал декодировать JSON
+$decodedJson = json_decode($cleanedJson, true);
+//var_export($decodedJson);
 
-$arrayJson = explode("\n", $cleanedJson); // Преобразовываю чистые данные в массив для прохождения foreach
-unset($arrayJson[0]);
-unset($arrayJson[1]);
-unset($arrayJson[2]); // Удаляю лишние элементы, которые не вычистились str_replace`ом
-foreach ($arrayJson as &$result) {
-    print_r(json_decode('"' . $result . '"') . "\n"); // Декодирую юникод-символы и вывожу 10 заголовков
+foreach ($decodedJson as $key) {
+      foreach ($key as $value) {
+          foreach ($value as $title) {
+              print_r($title . "\n");
+          }
+    }
 }
